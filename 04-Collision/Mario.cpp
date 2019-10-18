fe38620d356ccfe7a4299dc2be7bc9b743eee49e
NumbersZeros <thotpbank050699@gmail.com>
2019-10-18 22:19:08 +0700

    khởi tạo

---
+++ 04-Collision/Mario.cpp
@@ -0,0 +1,171 @@
+﻿#include <algorithm>
+#include "debug.h"
+
+#include "Mario.h"
+#include "Game.h"
+
+#include "Goomba.h"
+
+void CMario::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
+{
+	// Calculate dx, dy 
+	CGameObject::Update(dt);
+
+	// Simple fall down
+	vy += MARIO_GRAVITY*dt;		///gia tốc rớt
+
+	vector<LPCOLLISIONEVENT> coEvents;
+	vector<LPCOLLISIONEVENT> coEventsResult;
+
+	coEvents.clear();
+
+	// turn off collision when die 
+	if (state!=MARIO_STATE_DIE)
+		CalcPotentialCollisions(coObjects, coEvents); //khong tính toán nữa
+
+	// reset untouchable timer if untouchable time has passed
+	if ( GetTickCount() - untouchable_start > MARIO_UNTOUCHABLE_TIME)		//trạng thái đụng nắm baats tử
+	{
+		untouchable_start = 0;
+		untouchable = 0;
+	}
+
+	// No collision occured, proceed normally
+	if (coEvents.size()==0)			//nếu không có va chạm xảy ra thì mario sẽ di chuyeetn thoải mái (dang trên không trung)
+	{
+		x += dx; 
+		y += dy;
+	}
+	else
+	{
+		float min_tx, min_ty, nx = 0, ny;
+
+		FilterCollision(coEvents, coEventsResult, min_tx, min_ty, nx, ny);  //nx va ny là phương va chạm
+
+		// block 
+		x += min_tx*dx + nx*0.4f;		// nx*0.4f : need to push out a bit to avoid overlapping next frame  //va chạm chiều x 
+		y += min_ty*dy + ny*0.4f;		// va chạm vhieeuf y
+		
+		if (nx!=0) vx = 0; //chạm và không cho mario di chuyển 
+		if (ny!=0) vy = 0;
+
+		// Collision logic with Goombas
+		for (UINT i = 0; i < coEventsResult.size(); i++)
+		{
+			LPCOLLISIONEVENT e = coEventsResult[i];
+
+			if (dynamic_cast<CGoomba *>(e->obj)) // if e->obj is Goomba 
+			{
+				CGoomba *goomba = dynamic_cast<CGoomba *>(e->obj);
+
+				// jump on top >> kill Goomba and deflect a bit 
+				if (e->ny < 0)						//nếu vector ny nhỏ hơn 0 thì nó sẽ nhây
+				{
+					if (goomba->GetState()!= GOOMBA_STATE_DIE)
+					{
+						goomba->SetState(GOOMBA_STATE_DIE);
+						vy = -MARIO_JUMP_DEFLECT_SPEED;
+					}
+				}
+				else if (e->nx != 0)					// nếu va chạm theo chiều x thì mario sẽ nhỏ lại trong suốt thời gian đó 
+				{
+					if (untouchable==0)
+					{
+						if (goomba->GetState()!=GOOMBA_STATE_DIE)
+						{
+							if (level > MARIO_LEVEL_SMALL)			//giảm cấp độ con mario 
+							{
+								level = MARIO_LEVEL_SMALL;
+								StartUntouchable();				//trạng thái trong suốt 
+							}
+							else 
+								SetState(MARIO_STATE_DIE);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// clean up collision events
+	for (UINT i = 0; i < coEvents.size(); i++) delete coEvents[i];
+}
+
+void CMario::Render()
+{
+	int ani;
+	if (state == MARIO_STATE_DIE)
+		ani = MARIO_ANI_DIE;
+	else
+	if (level == MARIO_LEVEL_BIG)
+	{
+		if (vx == 0)
+		{
+			if (nx>0) ani = MARIO_ANI_BIG_IDLE_RIGHT;
+			else ani = MARIO_ANI_BIG_IDLE_LEFT;
+		}
+		else if (vx > 0) 
+			ani = MARIO_ANI_BIG_WALKING_RIGHT; 
+		else ani = MARIO_ANI_BIG_WALKING_LEFT;
+	}
+	else if (level == MARIO_LEVEL_SMALL)
+	{
+		if (vx == 0)
+		{
+			if (nx>0) ani = MARIO_ANI_SMALL_IDLE_RIGHT;
+			else ani = MARIO_ANI_SMALL_IDLE_LEFT;
+		}
+		else if (vx > 0)
+			ani = MARIO_ANI_SMALL_WALKING_RIGHT;
+		else ani = MARIO_ANI_SMALL_WALKING_LEFT;
+	}
+
+	int alpha = 255;
+	if (untouchable) alpha = 128;
+	animations[ani]->Render(x, y, alpha);
+
+	RenderBoundingBox();
+}
+
+void CMario::SetState(int state)
+{
+	CGameObject::SetState(state);
+
+	switch (state)
+	{
+	case MARIO_STATE_WALKING_RIGHT:
+		vx = MARIO_WALKING_SPEED;
+		nx = 1;
+		break;
+	case MARIO_STATE_WALKING_LEFT: 
+		vx = -MARIO_WALKING_SPEED;
+		nx = -1;
+		break;
+	case MARIO_STATE_JUMP: 
+		vy = -MARIO_JUMP_SPEED_Y;
+	case MARIO_STATE_IDLE: 
+		vx = 0;
+		break;
+	case MARIO_STATE_DIE:
+		vy = -MARIO_DIE_DEFLECT_SPEED;
+		break;
+	}
+}
+
+void CMario::GetBoundingBox(float &left, float &top, float &right, float &bottom)
+{
+	left = x;
+	top = y; 
+
+	if (level==MARIO_LEVEL_BIG)
+	{
+		right = x + MARIO_BIG_BBOX_WIDTH;
+		bottom = y + MARIO_BIG_BBOX_HEIGHT;
+	}
+	else
+	{
+		right = x + MARIO_SMALL_BBOX_WIDTH;
+		bottom = y + MARIO_SMALL_BBOX_HEIGHT;
+	}
+}
+
